9a5b74c07e19588f578281e4c93b2ee1d8ffb7a5
Gorthdzx <krislaksana123@gmail.com>
2024-05-08 07:38:53 +0700

    feat  : added views for home , blog and contact

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog Page</title>
+</head>
+<body>
+<div class="article1">
+    <div>
+        <h1>Artikel Jayablog</h1>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda
+            impedit laboriosam magnam praesentium! Architecto aspernatur impedit iure labore quasi?</h2>
+    </div>
+</div>
+<div class="article2">
+    <div>
+        <h1>Artikel Jayablog 2</h1>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda
+            impedit laboriosam magnam praesentium! Architecto aspernatur impedit iure labore quasi?</h2>
+    </div>
+</div>
+</body>
+</html>
