dc7cd641df8e136cfc84fa53211a911c3e05befd
Gorthdzx <krislaksana123@gmail.com>
2024-05-11 15:40:26 +0700

    feat  : rafactor code

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About me</title>
-    <link rel="stylesheet" href="/public/css/style.css">
-</head>
-<body>
-<h1>About Mahasiswa Gadungan</h1>
-<a href="/home">Home</a>
-<h1>Ketua kelas :<?= $nama;?> </h1>
-<script src="/public/js/script.js"></script>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{$title}}</x-slot:title>
+    <h3 class="text-center">
+        Blog About Page
+    </h3>
+</x-layout>
