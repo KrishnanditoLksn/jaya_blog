e74f71e107639ea106d77641ad7e5c93201bccd0
Gorthdzx <krislaksana123@gmail.com>
2024-05-11 15:49:10 +0700

    feat  : separated components

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}}</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+<body class="h-full">
+<div class="min-h-full">
+    <!--Get Navbar Component from folder -->
+    <x-navbar></x-navbar>
+    <x-header>{{$title}}</x-header>
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 bg-amber-400 text-center">
+            <!-- Your content -->
+            {{$slot}}
+        </div>
+    </main>
+</div>
+
+</body>
+</html>
